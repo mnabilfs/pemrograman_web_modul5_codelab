f14f101f27dd604634400e5c88b5f4b058de9aed
mnabilfs <mnabilfs22@gmail.com>
2025-12-10 14:48:49 +0700

    middleware + add post, put, delete

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,23 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('todos', TodoController::class);
-
+// Auth routes
 Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
     Route::get('me', [AuthController::class, 'me'])->middleware('auth:api');
 });
+
+
+// Todo routes with authentication
+Route::middleware(['auth:api'])->group(function () {
+    Route::post('todos', [TodoController::class, 'store']);
+    Route::put('todos/{todo}', [TodoController::class, 'update']);
+    Route::delete('todos/{todo}', [TodoController::class, 'destroy']);
+});
+
+
+// Public todo routes
+Route::get('todos', [TodoController::class, 'index']);
+Route::get('todos/{todo}', [TodoController::class, 'show']);
